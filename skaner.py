c041f2a3fca950d8a8541f907a5f0de5d64e2af6
mariapichor <mariapichor8@gmail.com>
2026-03-18 10:48:17 +0100

    skaner v4

--- skaner.py
+++ skaner.py
@@ -2,7 +2,8 @@ from token import Token
 def skaner(wyr):
     tokens = []
     n = len(wyr)
-    for i in range(n):
+    i=0
+    while i<n:
         napis = ""
         while(wyr[i].isdigit()):
             napis+=wyr[i]
@@ -10,7 +11,29 @@ def skaner(wyr):
         if napis!="":
             tokens.append(Token('LICZBA',int(napis)))
         
+        if (wyr[i].isalpha()):
+            napis = ""
+            while(wyr[i].isalpha()):
+                napis+=wyr[i]
+                i+=1
+            tokens.append(Token('ID',napis))
         if(wyr[i]=='+'):
             tokens.append(Token('PLUS','+'))
+        elif(wyr[i]=='-'):
+            tokens.append(Token('MINUS','-'))
+        elif(wyr[i]=='*'):
+            tokens.append(Token('MNOZENIE','*'))
+        elif(wyr[i]=='/'):
+            tokens.append(Token('DZIELENIE','/'))
+        elif(wyr[i]=='('):
+            tokens.append(Token('LNAWIAS','('))
+        elif(wyr[i]==')'):
+            tokens.append(Token('PNAWIAS',')'))
         elif(wyr[i]==" "):
             pass
+        else:
+            tokens.append(Token('ERR',wyr[i]))
+        i+=1
+    tokens.append(Token('EOF',''))
+    for token in tokens:
+        print(token.typ, token.wartosc)
