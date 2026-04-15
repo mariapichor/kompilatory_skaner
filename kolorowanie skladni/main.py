1119c39b47a83c0780bd980bc16caa68c7248ba1
Julia <juliamikula3@gmail.com>
2026-04-15 10:55:46 +0200

    main kolorowanie

--- kolorowanie skladni/main.py
+++ kolorowanie skladni/main.py
@@ -1,21 +1,37 @@
-from skaner import Skaner 
+import html
+from skaner import Skaner
 from typ import Typ
 
-wyr = "2+3*(76+8/3)+ 3*($9-3)"
-tokens = []
+KOLORY_TOKENOW = {
+    Typ.KEYWORD: "color: #ff79c6; font-weight: bold;",
+    Typ.OPERATOR: "color: #50fa7b;",
+    Typ.COMMENTS: "color: #6272a4; font-style: italic;",
+    Typ.STRING: "color: #f1fa8c;",
+    Typ.NUMBER: "color: #bd93f9;",
+    Typ.IDENTIFIER: "color: #f8f8f2;",
+    Typ.ERR: "color: #ff5555; text-decoration: underline;"
+}
+def generate_html(lista_tokenow,nazwa_p):
+    with open(nazwa_p,'w',encoding='utf-8') as f:
+        f.write('<html><body style="background: #282a36;"><pre>')
+        for t in lista_tokenow:
+            styl = KOLORY_TOKENOW.get(t.typ, "")
+            value = html.escape(str(t.wartosc))
+            f.write(f'<span style="{styl}">{value}</span>')
+        f.write('</pre></body></html>')
 
-i=0
-while i < len(wyr):
-    if wyr[i] == " ":
-        i += 1
-        continue
-    token = Skaner.skaner(wyr, i)
-    if token:
-        i = token.kolumna
-        tokens.append(token)
+if __name__=="__main__":
+    with open("example.txt","r")as f:
+        kod=f.read()
 
-for token in tokens:
-        if(token.typ != Typ.ERR):
-            print(token.typ.value, token.wartosc)
-        else:
-            print("Rozpoznano nieprawidłowy znak:", token.wartosc, " numer kolumny:", token.kolumna)
+    tokeny=[]
+    idx=0
+    while idx < len(kod):
+        token = Skaner.skaner(kod, idx)
+        tokeny.append(token)
+        idx = token.kolumna
+
+        if token.typ == Typ.EOF:
+            break
+
+    generate_html(tokeny, "wynik.html")
